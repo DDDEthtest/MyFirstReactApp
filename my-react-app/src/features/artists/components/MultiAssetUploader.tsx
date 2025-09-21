44e1d8cd3117c5a7a5812be9baabd3f254848c6e
Davi Miyake <davi.miyake@gmail.com>
2025-09-21 06:20:17 -0300

    artists/create: reorder asset card layout
    - Title above thumbnail
    - Upload button below thumbnail

--- my-react-app/src/features/artists/components/MultiAssetUploader.tsx
+++ my-react-app/src/features/artists/components/MultiAssetUploader.tsx
@@ -52,17 +52,15 @@ export const MultiAssetUploader: React.FC<Props> = ({ value, onChange }) => {
           const src = previews[def.key];
           return (
             <div key={def.key} className="asset-card">
-              <div className="asset-header">
-                <div className="asset-title">
-                  {def.label} {def.required && <span className="req">*</span>}
-                </div>
+              <div className="asset-title">
+                {def.label} {def.required && <span className="req">*</span>}
+              </div>
+              <div className={`thumb${src ? ' has-file' : ''}`}>
+                {src && <img src={src} alt={def.label} />}
+              </div>
+              <div className="upload">
                 <UploadWidget label={file ? 'Replace' : 'Upload'} onFileSelected={(f) => handlePick(def.key, f)} />
               </div>
-              {src && (
-                <div className="thumb has-file">
-                  <img src={src} alt={def.label} />
-                </div>
-              )}
             </div>
           );
         })}
